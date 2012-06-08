238588c479f2f883312db0df2d377f2dcb2fb504
James Paolantonio <james@jdubstudios.com>
2012-06-08 11:00:54 -0400

    Initial Commit

---
+++ openURLifAvailable/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  openURLifAvailable
+//
+//  Created by James Paolantonio on 6/8/12.
+//  Copyright (c) 2012 JDub Studios, llc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ openURLifAvailable/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  openURLifAvailable
+//
+//  Created by James Paolantonio on 6/8/12.
+//  Copyright (c) 2012 JDub Studios, llc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
